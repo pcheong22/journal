d16c9fafb81d2d620138ff51b829005f7330b51e
pcheong22 <pcheong22@gmail.com>
2026-05-10 22:17:15 +0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // This fixes the 'eval' error by allowing safe dynamic imports
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.output.globalObject = 'self'
+    }
+    return config
+  },
 }
+
 module.exports = nextConfig
