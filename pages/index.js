3fbf83783ab4f9d583a94f54efeab7c6cf3f0538
pcheong22 <pcheong22@gmail.com>
2026-05-15 21:11:02 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -541,7 +541,7 @@ export default function Dashboard() {
             <ChartComp type="equity" data={stats.cumulative} privacy={privacy} />
             <div style={{display:'grid',gridTemplateColumns:'1fr 1fr',gap:10,marginBottom:10}} className="g2">
               <ChartComp type="monthly"  data={stats.monthly} privacy={privacy} />
-              <ChartComp type="duration" data={stats.duration} />
+              <ChartComp type="duration" data={stats.duration} privacy={privacy} />
             </div>
             <div style={{display:'grid',gridTemplateColumns:'1fr 1fr',gap:10}} className="g2">
               <ChartComp type="direction"    longPnl={ov.long_pnl} shortPnl={ov.short_pnl} privacy={privacy} />
