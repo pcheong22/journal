a2a9251fd97f4fa4e0ae3fb1a4b64a9462399d27
pcheong22 <pcheong22@gmail.com>
2026-05-16 05:57:41 +0400

    Update Charts.js

--- components/Charts.js
+++ components/Charts.js
@@ -23,7 +23,7 @@ export default function ChartComp(props) {
   if (type==='monthly')      return <BarChart   title="MONTHLY P&L"      labels={props.data.map(d=>d.month_str)}   values={props.data.map(d=>Math.round(d.total_pnl))} height={170} privacy={privacy} />
   if (type==='duration')     return <BarChart   title="P&L BY DURATION"  labels={props.data.map(d=>d.bucket)}      values={props.data.map(d=>Math.round(d.total_pnl))} height={170} privacy={privacy} />
   if (type==='direction')    return <DirectionChart longPnl={props.longPnl} shortPnl={props.shortPnl} privacy={privacy} />
-  if (type==='distribution') return <DistChart trades={props.trades} privacy={privacy} />
+  if (type==='distribution') return  <DistChart       trades={props.trades} privacy={privacy} / >
   if (type==='symbolPnl')    return <HBarChart  title="P&L BY SYMBOL"    labels={props.data.map(d=>d.symbol)}      values={props.data.map(d=>Math.round(d.total_pnl))} height={270} privacy={privacy} />
   if (type==='symbolWr')     return <HBarChart  title="WIN RATE BY SYMBOL" labels={props.data.map(d=>d.symbol)}    values={props.data.map(d=>Math.round(d.win_rate*100))} height={270} isWr />
   if (type==='sessionPnl')   return <BarChart   title="SESSION P&L"      labels={props.data.map(d=>d.session)}     values={props.data.map(d=>Math.round(d.total_pnl))} height={200} privacy={privacy} />
